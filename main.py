b12ac50464338007a866d4d854c64a138faf2522
dprasant <dprasant@purdue.edu>
2024-06-30 02:15:21 -0500

    environment loaded

--- main.py
+++ main.py
@@ -4,3 +4,21 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.evaluation import evaluate_policy
 
+
+#Load Environment 
+env = gym.make("CartPole-v0")
+
+episodes = 5
+for episode in range(1, episodes+1):
+    state = env.reset()
+    done = False
+    score = 0 
+    
+    while not done:
+        env.render()
+        action = env.action_space.sample()
+        n_state, reward, done, info = env.step(action)
+        score+=reward
+    print('Episode:{} Score:{}'.format(episode, score))
+env.close()
+
