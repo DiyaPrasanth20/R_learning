2eddb5f1ba333c844e728a201a403e26b9f85a6e
dprasant <dprasant@purdue.edu>
2024-06-30 02:49:11 -0500

    issue 1 environment fixed

--- main.py
+++ main.py
@@ -7,24 +7,20 @@ from stable_baselines3.common.evaluation import evaluate_policy
 
 #Load Environment - state, action, reward, policy 
 
-env = gym.make("CartPole-v0")
+# Load Environment - state, action, reward, policy
+env = gym.make("CartPole-v1", render_mode='human')
 
 episodes = 5
 for episode in range(1, episodes + 1):
-    state = env.reset() #gives initial set of observations
+    state = env.reset()[0]  # For newer gym versions, reset() returns a tuple
     done = False
     score = 0
 
     while not done:
-        env.render() #shows environment 
-        action = env.action_space.sample() #generates random action (either 0 or 1)
-        n_state, reward, done, info = env.step(action) 
-        '''
-        pass through random action 
-        get next set of obserations [] len 4, reward, terminal state reached?, 
-
-        '''
-        score+=reward
+        env.render()  # shows environment
+        action = env.action_space.sample()  # generates random action (either 0 or 1)
+        n_state, reward, done, info = env.step(action)[:4]  # Handle additional return values if any
+        score += reward
     print('Episode:{} Score:{}'.format(episode, score))
 env.close()
 
