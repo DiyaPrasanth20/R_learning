efe7ebad38e0e8b4fe277b5a408c0583efe04e34
dprasant <dprasant@purdue.edu>
2024-06-30 02:28:22 -0500

    environment displayed

--- main.py
+++ main.py
@@ -5,20 +5,28 @@ from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.evaluation import evaluate_policy
 
 
-#Load Environment 
+#Load Environment - state, action, reward, policy 
+
 env = gym.make("CartPole-v0")
 
 episodes = 5
-for episode in range(1, episodes+1):
-    state = env.reset()
+for episode in range(1, episodes + 1):
+    state = env.reset() #gives initial set of observations
     done = False
-    score = 0 
-    
+    score = 0
+
     while not done:
-        env.render()
-        action = env.action_space.sample()
-        n_state, reward, done, info = env.step(action)
+        env.render() #shows environment 
+        action = env.action_space.sample() #generates random action (either 0 or 1)
+        n_state, reward, done, info = env.step(action) 
+        '''
+        pass through random action 
+        get next set of obserations [] len 4, reward, terminal state reached?, 
+
+        '''
         score+=reward
     print('Episode:{} Score:{}'.format(episode, score))
 env.close()
+
+
 
